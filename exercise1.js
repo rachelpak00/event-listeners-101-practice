433fa932fa303ff1acc98a969b20a95d41c7cd2b
ekoshiro16 <ekoshiro16@gmail.com>
2023-05-24 20:20:53 -0700

    edited typos

--- exercise1.js
+++ exercise1.js
@@ -1,5 +1,7 @@
 // In this exercise, we will design a "game" where the objective is to click a button and earn as many clicks as possible.
 
+// Note that all of these exercises do not have any linking between the JS and HTML files. Be sure to practice linking the two files before starting your actual coding work below. 
+
 // First, we'll need our HTML "cooking ingredients". Fetch the elements that we will be modifying or manipulating in this exercise. Take a close look at the HTML file and think about what user actions are occurring on what element, and what elements are changing in response to those user actions. 
 
 // Next, once we have our HTML elements selected, or imported, into our JS file, we can attach an event listener to them. Attach a "click" event listener to the button that, when triggered, will run a callback function that increases the total clicks element by 1 with each click. 
---
+++ exercise4.js
@@ -0,0 +1,5 @@
+// You now have a 3x3 grid. We want to configure this grid such that when the left 3 cells are clicked, the colors turn red, when the middle 3 cells are clicked, the color turns green, and when the last 3 cells are clicked, the color turns blue. 
+
+// Write some JS code below such that event listeners are attached to all the correct elements following the specifications above.
+
+// Hint: Some sort of loop may be helpful to you in this process!
