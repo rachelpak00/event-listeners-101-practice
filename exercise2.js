368c1d30203e87541af6d91c0f102b458fd11222
ekoshiro16 <ekoshiro16@gmail.com>
2023-05-24 19:31:37 -0700

    edited exercises

---
+++ exercise2.js
@@ -0,0 +1,7 @@
+// Let's also practice the different type of button press type of user events that can occur. 
+
+// First, we'll do the regular click event. On the #circle-one element, set an event listener that when you click it, will turn its background color blue.
+
+// Next, we'll do the event when any key is pressed on the keyboard. On the #circle-two element, set an event listener that when you press any key on the keyboard, will turn its background color green.
+
+// Lastly, we'll do the event when the user releases a key on the keyboard. On the #circle-three element, set an event listener that when you release any key on the keyboard, will turn its background color red. 
